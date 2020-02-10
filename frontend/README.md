2b71d52c5c67321550bbb03a62c2c9d6068296ac
Ole August <oleastole@gmail.com>
2020-02-10 19:04:49 +0100

    Set up the initial frontend boilerplate

---
+++ frontend/README.md
@@ -0,0 +1,17 @@
+# Skikkelig Fancy Hotell
+> Front-end in Next.js
+
+## Setup
+
+```
+$ git clone ...
+$ cd frontend
+$ yarn
+```
+
+## Development
+
+```
+$ yarn dev
+$ # Open http://localhost:3000
+```
---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Link from "next/link";
+
+// import Layout from "./Layout";
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Skikkelig Fancy Hotell</h1>
+        <Link href="booking">
+          <a>Book Hotel</a>
+        </Link>
+      </div>
+    )
+  }
+}
