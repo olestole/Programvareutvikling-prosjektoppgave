6cd202ad82dd12be4b08ea4d7f6d623e09477244
SondreODahl <sondreo.dahl@gmail.com>
2020-02-11 12:45:41 +0100

    [front-end] fixed path from homepage to rooms

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Link from "next/link";
+import Link from 'next/link';
 
 // import Layout from "./Layout";
 
@@ -8,10 +8,10 @@ export default class Homepage extends Component {
     return (
       <div>
         <h1>Skikkelig Fancy Hotell</h1>
-        <Link href="booking">
+        <Link href="rooms">
           <a>Book Hotel</a>
         </Link>
       </div>
-    )
+    );
   }
 }
