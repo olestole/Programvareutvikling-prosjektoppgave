83ee874775a3010fe81be5b5999df9e403fee2a8
Ole August <oleastole@gmail.com>
2020-02-18 00:52:38 +0100

    Fix bug in last commit

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Navbar from '../../components/Navbar';
-import LoginForm from '../../components/LoginForm';
+import Navbar from '../components/Navbar';
+import LoginForm from '../components/LoginForm';
 
 const useStyles = makeStyles({
   root: {
