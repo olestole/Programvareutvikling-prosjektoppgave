c26f7bba75ab5414216ace22153acc718dad2029
SondreODahl <sondreo.dahl@gmail.com>
2020-02-19 11:27:03 +0100

    Fix linting

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
