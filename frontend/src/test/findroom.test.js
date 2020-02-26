490b1bc3cf6c25ee22c1039d6b7d5c0f42ec7957
Sondre Dahl <sondreo.dahl@gmail.com>
2020-02-26 16:25:41 +0100

    add fix for linting issue

--- frontend/src/test/findroom.test.js
+++ frontend/src/test/findroom.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import FindRoom from '../components/FindRoom';
-import FindRoom from '../components/FindRoom';
 
 it('renders avatar as expected', () => {
   const tree = renderer.create(<FindRoom />).toJSON();
