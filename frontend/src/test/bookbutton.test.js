8895f5087e322f5f3937e5a1774faf20a1a2203f
Sondre Dahl <sondreo.dahl@gmail.com>
2020-02-27 16:19:26 +0100

    remove bookbutton test

--- frontend/src/test/bookbutton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import BookButton from '../components/BookButton';
-
-test('renders Finn ditt rom!', () => {
-  const { getByText } = render(<BookButton />);
-  const linkElement = getByText(/Finn ditt rom!/);
-  expect(linkElement).toBeInTheDocument();
-});
