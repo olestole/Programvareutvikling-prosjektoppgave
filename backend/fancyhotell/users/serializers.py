cc80e40ebbdefb30f7c244361240694b68a3ab57
Sondre Dahl <sondreo.dahl@gmail.com>
2020-03-22 18:09:58 +0100

    add user update

--- backend/fancyhotell/users/serializers.py
+++ backend/fancyhotell/users/serializers.py
@@ -1,5 +1,6 @@
 from fancyhotell.users.models import Customer, Address, User
 from rest_framework import serializers
+from django.db import transaction
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -59,3 +60,59 @@ class UserCreateSerializer(serializers.ModelSerializer):
         user.customer = customer
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            instance.email = validated_data.get("email", instance.email)
+            instance.phone = validated_data.get("phone", instance.phone)
+            instance.save()
+            # Update address
+            address_data = validated_data.get("address", None)
+            if address_data is not None:
+                address = instance.address
+                address.street_name = address_data.get(
+                    "street_name", instance.street_name
+                )
+                address.street_number = address_data.get(
+                    "street_number", instance.street_number
+                )
+                address.city = address_data.get(
+                    "city", instance.city
+                )
+                address.postal_code = address_data.get(
+                    "postal_code", instance.postal_code
+                )
+                address.country = address_data.get(
+                    "country", instance.country
+                )
+                # Update address fields as above city, postal_code, country
+                # ...
+                instance.save()
+            customer_data = validated_data.get("customer", None)
+            if customer_data is not None:
+                customer = instance.customer
+                # same as above for all fields..."id" "email", "first_name",
+                # "last_name","phone","address", "password",
+                customer.id = customer_data.get(
+                    "id", instance.id
+                )
+                customer.email = customer_data.get(
+                    "email", instance.email
+                )
+                customer.first_name = customer_data.get(
+                    "first_name", instance.first_name
+                )
+                customer.last_name = customer_data.get(
+                    "last_name", instance.last_name
+                )
+                customer.phone = customer_data.get(
+                    "phone", instance.phone
+                )
+                customer.address = customer_data.get(
+                    "address", instance.address
+                )
+                customer.password = customer_data.get(
+                    "password", instance.password
+                )
+                customer.save()
+        return instance
